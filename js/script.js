3d4d846b739ae73eee09f44f5c20580ca1458286
szumiecam <96595903+szumiecam@users.noreply.github.com>
2022-10-14 23:25:27 +0200

    variable pokemonList with objects

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 
-alert('Hello Pokemons');
+let pokemonList = [];
 
-let favoriteFood = 'Pumpkin Ghost in the sose'
-document.write(favoriteFood);
+let pokemon1 = {name:'Beedrill', height:" 3' 03 ",types:['bug', 'poison']};
+let pokemon2 = {name:'Butterfree', height:" 3'07 ",types:['bug', 'flying']};
+let pokemon3 = {name:'Pikachu', height:"1' 04 ",types:['elektric']};
+let pokemon4 = {name:'Magneton', height:"3' 03 ",types:['electric', 'steel']};
+let pokemon5 = {name:'Jynx', height:"4' 07 ",types:['ice', 'psychic']};
+
+pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5];

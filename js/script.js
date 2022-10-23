e206ae75d346340a3d76471b39112ead6f0c4d63
annamaria <szumiecam@gmail.com>
2022-10-23 17:54:27 +0200

    Iterating List
    changed style  of PokemonList / Iterating the List and print result

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 
-let pokemonList = [];
+let pokemonList = [
+    {name:'Beedrill', height:" 3' 03 ",types:['bug', 'poison']},
+    {name:'Butterfree', height:" 3'07 ",types:['bug', 'flying']},
+    {name:'Pikachu', height:"1' 04 ",types:['elektric']},
+    {name:'Magneton', height:"3' 03 ",types:['electric', 'steel']},
+    {name:'Jynx', height:"4' 07 ",types:['ice', 'psychic']},
+    ];
 
-let pokemon1 = {name:'Beedrill', height:" 3' 03 ",types:['bug', 'poison']};
-let pokemon2 = {name:'Butterfree', height:" 3'07 ",types:['bug', 'flying']};
-let pokemon3 = {name:'Pikachu', height:"1' 04 ",types:['elektric']};
-let pokemon4 = {name:'Magneton', height:"3' 03 ",types:['electric', 'steel']};
-let pokemon5 = {name:'Jynx', height:"4' 07 ",types:['ice', 'psychic']};
+    // Iterating the List and print result
 
-pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5];
+    for (let i=0; i < pokemonList.length; i++) {
+
+        if (pokemonList[i].height > "3' 03 ") {
+            document.write(pokemonList[i].name + " is " + pokemonList[i].height +  " tall, " + "Waw - that is BIG!");
+        }
+        else{
+            document.write(pokemonList[i].name + " is " + pokemonList[i].height + " tall, ");
+        }
+    }
+
+
+
+
+     

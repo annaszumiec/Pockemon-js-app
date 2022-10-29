7e0cc715a74c7b2be566d3d83fc04d8fbd07b3ac
annamaria <szumiecam@gmail.com>
2022-10-29 21:21:14 +0200

    changed forEach function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 
 let pokemonList = [
-    {name:'Beedrill', height:" 3' 03 ",types:['bug', 'poison']},
-    {name:'Butterfree', height:" 3'07 ",types:['bug', 'flying']},
-    {name:'Pikachu', height:"1' 04 ",types:['elektric']},
-    {name:'Magneton', height:"3' 03 ",types:['electric', 'steel']},
-    {name:'Jynx', height:"4' 07 ",types:['ice', 'psychic']},
+    {name:'Beedrill', height:" 3' 03 ",types:[' bug ', ' poison ']},
+    {name:'Butterfree', height:" 3'07 ",types:[' bug ', ' flying ']},
+    {name:'Pikachu', height:"1' 04 ",types:[' elektric ']},
+    {name:'Magneton', height:"3' 03 ",types:[' electric ', ' steel ']},
+    {name:'Jynx', height:"4' 07 ",types:[' ice ', ' psychic ']},
     ];
 
 pokemonList.forEach(function(pokemon){
-    document.write(pokemon.name + ' is ' + pokemon.height + ' tall ' + ' and type :'+pokemon.types);
+    if (pokemon.height > "3' 03 ") {
+        document.write(pokemon.name + " is " + pokemon.types + ' and ' + pokemon.height +  " tall , " + "Waw - that is BIG!");
+        }
+        else{
+              document.write(pokemon.name + " is " + pokemon.types + 'and' + pokemon.height + " tall , ");
+       }
 });
 
 
